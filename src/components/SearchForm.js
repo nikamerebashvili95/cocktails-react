3689bf6e464bef0be0f80f5ca1aac51847e3c81f
nikamerebashvili95 <nikamerebashvili95@gmail.com>
2023-08-18 06:56:22 -0700

    cocktails Search Form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,11 +2,31 @@ import React from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const { searchTerm } = useGlobalContext();
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search Your Favourite Cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
